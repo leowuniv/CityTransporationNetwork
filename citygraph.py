bfdd48fb1bab0ca8e901836529475b8ab37212ff
Leo <leowuniv@gmail.com>
2025-05-05 20:09:50 -0700

    Update citygraph.py

--- citygraph.py
+++ citygraph.py
@@ -1,1 +1,31 @@
 # Graph
+
+import heap
+
+class Graph:
+  def __init__(self):
+    # Dictionary to store adjacency list with weights
+    self.adj_list = {}
+    self.edges = []
+    
+  def addVertex(self, vertex):
+    # Add a vertex if it does not already exist
+    if vertex not in self.adj_list:
+      self.adj_list[vertex] = []
+
+  def addEdge(self, vertex1, vertex2, weight=1):
+    # Add weighted edge between V1 and V2 (Undirected Graph)
+    if vertex1 in self.adj_list and vertex2 in self.adj_list:
+      self.adj_list[vertex1].append([vertex2, weight])
+      self.adj_list[vertex2].append([vertex1, weight])
+      self.edges.append((weight, vertex1, vertex2))
+    else:
+      raise ValueError("Both vertices must exist in the graph.")
+
+  # def
+
+  def display(self):
+    # Display adjacency list
+    for vertex, edges in self.adj_list.items():
+      print(f"{vertex}: {edges}")
+
