607199e512fd2e420e9753bcb0cc2d11824c3ab2
Leo <leowuniv@gmail.com>
2025-05-05 20:26:21 -0700

    Update citygraph.py

--- citygraph.py
+++ citygraph.py
@@ -3,17 +3,28 @@
 import heap
 
 class Graph:
+  '''
+  A class that uses adjacency lists or matrices to keep track of edges
+  '''
   def __init__(self):
     # Dictionary to store adjacency list with weights
     self.adj_list = {}
     self.edges = []
     
   def addVertex(self, vertex):
+    '''
+    Adds a vertex to graph
+    '''
     # Add a vertex if it does not already exist
     if vertex not in self.adj_list:
       self.adj_list[vertex] = []
 
+  # Ensure that your graph can handle String identifiers for vertices.
+
   def addEdge(self, vertex1, vertex2, weight=1):
+    '''
+    Adds edge
+    '''
     # Add weighted edge between V1 and V2 (Undirected Graph)
     if vertex1 in self.adj_list and vertex2 in self.adj_list:
       self.adj_list[vertex1].append([vertex2, weight])
@@ -30,6 +41,19 @@ class Graph:
       print(f"{vertex}: {edges}")
 
   def bfs():
+    '''
+    This class for BFS prints out the order of landmarks visited
+    '''
 
   def dfs():
+    '''
+    This class for DFS is using a stack to print out the order of landmarks visited
+    '''
+
+def main(): # tester for city network
+  # Create a method to instantiate a sample city network with at least 10 landmarks and 15 roads.
+  # Demonstrate both BFS and DFS traversals starting from a given landmark.
+
+if __name__ == "__main__":
+    main()
 
