f3deccac7a967415a3a62a82cf6bdefaac1a184c
Leo <leowuniv@gmail.com>
2025-05-09 00:53:53 -0700

    Update citygraph.py

--- citygraph.py
+++ citygraph.py
@@ -48,12 +48,31 @@ class Graph:
     This class for BFS prints out the order of landmarks visited
     '''
     visited = []
+    '''
+    1. Start by creating an empty set and Queue of vertices to visit,
+    putting the source vertex in the queue and setting it as visited in
+    the array.
+    2.While the Queue is not empty:
+    1. Take the front item of the queue and print it
+    2. Add all its unvisited adjacent vertices to the back of the queue while
+    also marking them as visited to avoid adding them again.
+    '''
 
   def dfs(self):
     '''
     This class for DFS is using a stack to print out the order of landmarks visited
     '''
     visited = []
+    '''
+    1. Create new stack and Boolean array, start at source vertex
+    and add it to stack.
+    2. While the Stack is not empty:
+    1. Pop a node from the stack to use
+    2. If that node has not been visited, mark it as being visited and print
+    the node
+    3. For all non-visited adjacent nodes to the current node, push them
+    onto the stack
+    '''
 
 def main(): # tester for city network
   # Create a method to instantiate a sample city network with at least 10 landmarks and 15 roads.
